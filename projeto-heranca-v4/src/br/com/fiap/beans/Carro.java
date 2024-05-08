0aff23117b7aa2a8c05afae0ee1df8e44ce8a51c
carmipa <pabcarminati@gmail.com>
2024-05-07 21:43:25 -0300

    herança 4

---
+++ projeto-heranca-v4/src/br/com/fiap/beans/Carro.java
@@ -0,0 +1,32 @@
+package br.com.fiap.beans;
+
+public class Carro  extends Veiculo{
+	
+	private String volante;
+
+	public Carro() {
+		super();
+	}
+
+	public Carro(String placa, String marca, String modelo, int ano, double valor, String volante) {
+		super(placa, marca, modelo, ano, valor);
+		this.volante = volante;
+	}
+
+	public String getVolante() {
+		return volante;
+	}
+
+	public void setVolante(String volante) {
+		this.volante = volante;
+	}
+
+	@Override
+	public String toString() {
+		return "Carro [getVolante()=" + getVolante() + ", getPlaca()=" + getPlaca() + ", getMarca()=" + getMarca()
+				+ ", getModelo()=" + getModelo() + ", getAno()=" + getAno() + ", getValor()=" + getValor() + "]";
+	}
+	
+	
+
+}
