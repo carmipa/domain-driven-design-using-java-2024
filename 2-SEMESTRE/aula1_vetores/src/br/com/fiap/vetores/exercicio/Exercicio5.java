797514238e53a4f86d101de200f9f36fe044bee3
carmipa <pabcarminati@gmail.com>
2024-08-08 09:58:03 -0300

    array

---
+++ 2-SEMESTRE/aula1_vetores/src/br/com/fiap/vetores/exercicio/Exercicio5.java
@@ -0,0 +1,15 @@
+package br.com.fiap.vetores.exercicio;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Crie um programa que leia um vetor de 7 inteiros e determine se
+		 * o vetor é um palindromo(ou seja é igual ao seu reverso)
+		 * 
+		 * */
+
+	}
+
+}
